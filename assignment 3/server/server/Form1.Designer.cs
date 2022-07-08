c4ab6f52c1894bcd7fb00ee6f234bac15daa0c49
ace sono <93066951+acesono@users.noreply.github.com>
2022-07-08 21:06:20 +0300

    assignment 3

---
+++ assignment 3/server/server/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_oname = new System.Windows.Forms.TextBox();
+            this.txt_count = new System.Windows.Forms.TextBox();
+            this.txt_number = new System.Windows.Forms.TextBox();
+            this.txt_inumber = new System.Windows.Forms.TextBox();
+            this.txt_price = new System.Windows.Forms.TextBox();
+            this.addbutton = new System.Windows.Forms.Button();
+            this.cancle_button = new System.Windows.Forms.Button();
+            this.txt_date = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_oname
+            // 
+            this.txt_oname.Location = new System.Drawing.Point(33, 133);
+            this.txt_oname.Name = "txt_oname";
+            this.txt_oname.Size = new System.Drawing.Size(729, 20);
+            this.txt_oname.TabIndex = 1;
+            // 
+            // txt_count
+            // 
+            this.txt_count.Location = new System.Drawing.Point(33, 202);
+            this.txt_count.Name = "txt_count";
+            this.txt_count.Size = new System.Drawing.Size(100, 20);
+            this.txt_count.TabIndex = 2;
+            // 
+            // txt_number
+            // 
+            this.txt_number.Location = new System.Drawing.Point(33, 59);
+            this.txt_number.Name = "txt_number";
+            this.txt_number.Size = new System.Drawing.Size(226, 20);
+            this.txt_number.TabIndex = 4;
+            // 
+            // txt_inumber
+            // 
+            this.txt_inumber.Location = new System.Drawing.Point(550, 59);
+            this.txt_inumber.Name = "txt_inumber";
+            this.txt_inumber.Size = new System.Drawing.Size(212, 20);
+            this.txt_inumber.TabIndex = 5;
+            // 
+            // txt_price
+            // 
+            this.txt_price.Location = new System.Drawing.Point(159, 202);
+            this.txt_price.Name = "txt_price";
+            this.txt_price.Size = new System.Drawing.Size(100, 20);
+            this.txt_price.TabIndex = 6;
+            // 
+            // addbutton
+            // 
+            this.addbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addbutton.Location = new System.Drawing.Point(184, 322);
+            this.addbutton.Name = "addbutton";
+            this.addbutton.Size = new System.Drawing.Size(116, 37);
+            this.addbutton.TabIndex = 7;
+            this.addbutton.Text = "ADD";
+            this.addbutton.UseVisualStyleBackColor = true;
+            this.addbutton.Click += new System.EventHandler(this.addbutton_Click);
+            // 
+            // cancle_button
+            // 
+            this.cancle_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancle_button.Location = new System.Drawing.Point(452, 322);
+            this.cancle_button.Name = "cancle_button";
+            this.cancle_button.Size = new System.Drawing.Size(116, 37);
+            this.cancle_button.TabIndex = 8;
+            this.cancle_button.Text = "CANCLE";
+            this.cancle_button.UseVisualStyleBackColor = true;
+            this.cancle_button.Click += new System.EventHandler(this.cancle_button_Click);
+            // 
+            // txt_date
+            // 
+            this.txt_date.Location = new System.Drawing.Point(301, 59);
+            this.txt_date.Name = "txt_date";
+            this.txt_date.Size = new System.Drawing.Size(200, 20);
+            this.txt_date.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(297, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "object name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(164, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "price";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(29, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "count";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(546, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(146, 20);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "inventory number";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_date);
+            this.Controls.Add(this.cancle_button);
+            this.Controls.Add(this.addbutton);
+            this.Controls.Add(this.txt_price);
+            this.Controls.Add(this.txt_inumber);
+            this.Controls.Add(this.txt_number);
+            this.Controls.Add(this.txt_count);
+            this.Controls.Add(this.txt_oname);
+            this.Name = "Form1";
+            this.Text = "SERVER";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txt_oname;
+        private System.Windows.Forms.TextBox txt_count;
+        private System.Windows.Forms.TextBox txt_number;
+        private System.Windows.Forms.TextBox txt_inumber;
+        private System.Windows.Forms.TextBox txt_price;
+        private System.Windows.Forms.Button addbutton;
+        private System.Windows.Forms.Button cancle_button;
+        private System.Windows.Forms.DateTimePicker txt_date;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ assignment 3/server/server/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cancle_button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void addbutton_Click(object sender, EventArgs e)
+        {
+            product p = new product {number=double.Parse(txt_number.Text),date=txt_date.Text,inventory_number=double.Parse(txt_inumber.Text),object_name=txt_oname.Text,count=double.Parse(txt_count.Text),price=double.Parse(txt_price.Text) };
+            p.save();
+            MessageBox.Show("Saved succsusfully!");
+        }
+    }
+}
